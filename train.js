54809e8e35f16f5572ff36b0f5531e83cc1b0d04
Jacob <juraboev0115@naver.com>
2026-04-24 18:54:39 +0900

    add train js mitask

--- train.js
+++ train.js
@@ -9,22 +9,50 @@ const list = [
   "endi dam oling, foydasi yoq endi", // 60
 ];
 
-async function maslahatBering(a) {
-  if (typeof a !== "number") throw new Error("insert a number");
-  else if (a <= 20) return list[0];
-  else if (a > 20 && a <= 30) return list[1];
-  else if (a > 30 && a <= 40) return list[2];
-  else if (a > 40 && a <= 50) return list[3];
-else if (a > 50 && a <= 60) return list[4];
-else {
-  return list[5];
-  // setTimeout(function () {
-  //   return list[5];
-  // }, 5000);
-}
+// async function maslahatBering(a) {
+//   if (typeof a !== "number") throw new Error("insert a number");
+//   else if (a <= 20) return list[0];
+//   else if (a > 20 && a <= 30) return list[1];
+//   else if (a > 30 && a <= 40) return list[2];
+//   else if (a > 40 && a <= 50) return list[3];
+//   else if (a > 50 && a <= 60) return list[4];
+//   else {
+//     setInterval(function () {
+//       callback(null, list[5]);
+//     }, 1000);
+//   }
+// }
 
 
-// then/catch
+console.log("passed here 0");
+
+//maslahatBering(70, (err, data) => {
+//  if (err) console.log("ERROR:", err);
+//  else {
+//    console.log(data);
+//  }
+//});
+
+console.log("passed here 1");
+
+// ASYNC function
+// async function maslahatBering(a) {
+//   if (typeof a !== "number") throw new Error("insert a number");
+//   else if (a <= 20) return list[0];
+//   else if (a > 20 && a <= 30) return list[1];
+//   else if (a > 30 && a <= 40) return list[2];
+//   else if (a > 40 && a <= 50) return list[3];
+//   else if (a > 50 && a <= 60) return list[4];
+//   else {
+//     return new Promise((resolve, reject) => {
+//       setTimeout(() => {
+//         resolve(list[5]);
+//       }, 5000);
+//     });
+//   }
+// }
+
+// call via then/catch
 // console.log("passed here 0");
 // maslahatBering(65)
 //   .then((data) => {
@@ -36,16 +64,22 @@ else {
 // console.log("passed here 1");
 
 
-// async/await
-async function run() {
-  let javob = await maslahatBering(20);
-  console.log(javob);
 
-  javob = await maslahatBering(31);
-  console.log(javob);
+function countLetter(letter, talaba) {
+    let count = 0;
 
-  javob = await maslahatBering(41);
-  console.log(javob);
+    for(let i = 0; i < talaba.length; i++) {
+        if(talaba[i] === letter) {
+            count++;
+        }
+    }
+
+    return count;
 }
 
-run();
+console.log(countLetter("e", "engineer") );
+
+
+
+
+
