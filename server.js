947fbd29a59a92ab0f769dfededa434fdfabefcb
Jacob <juraboev0115@naver.com>
2026-04-26 20:55:09 +0900

    portfolia ejs

--- server.js
+++ server.js
@@ -1,28 +1,48 @@
-console.log("Web Serverni boshlash");
-
 const express = require("express");
 const app = express();
-const http = require("http");
+const fs = require("fs");
 
-// 1: Kirish code
+let user;
+
+// JSON file o‘qish
+fs.readFile("database/user.json", "utf8", (err, data) => {
+  if (err) {
+    console.log("ERROR:", err);
+  } else {
+    user = JSON.parse(data);
+  }
+});
+
+// 1. Middleware
 app.use(express.static("public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// 2: Session code
-
-// 3: Views code
+// 2. Views
 app.set("views", "views");
 app.set("view engine", "ejs");
 
-// 4: Routing code
-app.get("/", function (req, res) {
-    res.end("<h1>HELLO WORLD by Jacob</h1>");
+// 3. Routing
+
+// Author portfolio page
+app.get("/author", (req, res) => {
+  res.render("author", { user: user });
 });
 
-const server = http.createServer(app);
-let PORT = 3000;
+// Home page
+app.get("/", (req, res) => {
+  res.render("harid");
+});
 
-server.listen(PORT, function () {
-    console.log(`The server is running successfully on port: ${PORT}`);
+// Form create item
+app.post("/create-item", (req, res) => {
+  console.log(req.body);
+  res.send("Item created successfully");
+});
+
+// 4. Server start
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, function () {
+  console.log(`Server is running on port ${PORT}`);
 });
